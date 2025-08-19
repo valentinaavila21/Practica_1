b7dc96a2fc5608c0f5ddd1b5d6f62ec7f202ec4a
Valentina Ávila P. <valentina.avila.p@ug.uchile.cl>
2025-08-19 01:49:40 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+
+  let centerx = width / 2; 
+  let centery = height / 2; 
+  let bigRadius = 100; 
+  let smallDiameter = 100;
+
+  // Círculo grande centrado
+  fill(255);
+  stroke(0);
+  circle(centerx, centery, bigRadius * 2);
+
+  // Vector desde el centro hacia el mouse
+  let dx = mouseX - centerx;
+  let dy = mouseY - centery;
+  let distance = sqrt(dx * dx + dy * dy);
+
+  let maxDistance = bigRadius - smallDiameter / 2;
+  let angle = atan2(dy, dx);
+
+  let smallX, smallY;
+  let touchingEdge = false;
+
+  if (distance < maxDistance) {
+    smallX = mouseX;
+    smallY = mouseY;
+  } else {
+    smallX = centerx + cos(angle) * maxDistance;
+    smallY = centery + sin(angle) * maxDistance;
+    touchingEdge = true;
+  }
+
+  // Círculo pequeño con cambio de color
+  if (touchingEdge) {
+    fill(255,0,0); // rosado si toca el borde
+  } else {
+    fill(0); // negro si está dentro
+  }
+
+  stroke(0);
+  circle(smallX, smallY, smallDiameter);
+}
